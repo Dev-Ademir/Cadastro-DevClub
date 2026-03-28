bb6bd2d2c284c0472115cfa6cf1245dbe1b8cf37
KyrraH22 <ademir.voigt@ulbra.edu.br>
2026-03-28 15:26:45 -0300

    feat: inicializa projeto React com styled-components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Title } from "./styles";
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Title>Cadastro</Title>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+/**
+  quando quiser exportar um app anas usa um "export default App"
+  quando quiser exportar vários apps usa o export na frente do function "export function App(){}"
+ */
